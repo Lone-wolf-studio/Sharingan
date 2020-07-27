b37d8f1229a0bffb5a4568a367ff68d7e63d53b2
codyowl <codyowl@gmail.com>
2020-07-27 21:46:19 +0530

    finding models.py dir path finder logic added :rocket:

--- sharingan.py
+++ sharingan.py
@@ -10,12 +10,15 @@ arguments = parser.parse_args()
 project_path = arguments.project_path
 
 class ModelsConverter(object):
-	# method to list out all models.py files path
+	# method to get models.py files path
 	def get_all_dir_path(self):
-		self.dir_path_list = [path for path, dir_name, file in os.walk(project_path)]
+		self.dir_path_list = [path for path, dir_name, file in os.walk(project_path) for file_name in file if file_name=="models.py"]
 		return self.dir_path_list
 
+	
 if __name__ == "__main__":
 	convert = ModelsConverter()
-	dir_path_list = convert.get_all_dir_path()		
+	all_dirs_path = convert.get_all_dir_path()
+	print(all_dirs_path)
+	
 	
